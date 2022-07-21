433998915a24c8c09b01db260880d81b660a63c5
tishanjegels <95338764+tishanjegels@users.noreply.github.com>
2022-07-21 23:05:42 +0200

    Worked on login page
    html and css

--- js/home.js
+++ js/home.js
@@ -13,7 +13,7 @@ import { MovieList } from "netflixapp/classes/movie-list.js";
 
 // start with Vue
 
-const {createApp} = window.Vue;
+const { createApp } = window.Vue;
 
 const component = {
     data() {
@@ -32,10 +32,24 @@ const component = {
             localStorage.removeItem();
             window.location.href = "../login.html"
         },
-        // wehn the button is click ad movie to watch list 
+        // when the button is click ad movie to watch list 
 
         watchBTN() {
             window.location.href = "../watchlist.html"
+        }
+    },
+    computed: {
+
+        // computed property for coming soon movies 
+        comingSoon() {
+            return this.movieList.filter((movie) => {
+                return movie.comingSoon === true;
+            })
+        },
+        availableMovie() {
+            return this.movieList.filter((movie) => {
+                return movie.comingSoon === false;
+            })
         }
     }
 
